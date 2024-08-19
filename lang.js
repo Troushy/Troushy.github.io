c8ba166858c3253cad1566a9697fad8f27f96a00
UralStuff <dejdevatarovzena@gmail.com>
2024-08-19 11:44:18 +0500

    vv5.12.0.2-12

--- lang.js
+++ lang.js
@@ -1,1 +1,25 @@
-alert(location.results[0].address_components[i].long_name);
+function displayPosition(position) {
+    var GEOCODING = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + '%2C' + position.coords.longitude + '&language=ru';
+  
+    $.getJSON(GEOCODING).done(function(location) {
+  
+      $('#address').html(location.results[0].formatted_address);
+      $('#latitude').html(position.coords.latitude);
+      $('#longitude').html(position.coords.longitude);
+  
+      // в location.results[0] содержится больше всего информации об адресе
+      for (var i = 0; i < location.results[0].address_components.length; i++) {
+         switch(location.results[0].address_components[i].types[0]) {
+           case 'locality':
+             $('#city').html(location.results[0].address_components[i].long_name);
+             break;
+           case 'administrative_area_level_1':
+             $('#state').html(location.results[0].address_components[i].long_name);
+             break;
+           case 'country':
+             $('#country').html(location.results[0].address_components[i].long_name);
+             break;
+         }
+      }
+    })
+  }

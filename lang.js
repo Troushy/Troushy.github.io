4c50f8f34df0f308037e5e9e6d87a82cbd9463fc
UralStuff <dejdevatarovzena@gmail.com>
2024-08-19 12:48:45 +0500

    v5.12.0.3-03

--- lang.js
+++ lang.js
@@ -3,5 +3,7 @@ window.onload = function () {
 	document.getElementById("country").innerHTML = o.country;
     if (o.country == "Россия" || o.country == "Беларусь"){
         window.location.href='ru.html';
+    } else {
+        window.location.href='en.html';
     }
 }

ba350c2e54e28c7e0f8560eebc40f2af3c2c08e1
UralStuff <dejdevatarovzena@gmail.com>
2024-09-02 01:38:23 +0500

    v5.12.1.1-08

--- langFiles.js
+++ langFiles.js
@@ -1,8 +1,5 @@
 var select = document.getElementById("language");
 select.addEventListener('change', function (e) {
     // alert(e.target.value + '.html');
-    var yes = confirm("Are you sure?/Вы уверены?");
-    if (yes)
-        window.location.href = e.target.value + '.html';
-    else {}
+    window.location.href = e.target.value + '.html';
 })

136dd6a93aff8091466cb08e0dc6a1aafdc0ed48
UralStuff <dejdevatarovzena@gmail.com>
2024-09-02 02:35:22 +0500

    v5.12.1.2-01

--- june/lang.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-	const o = ymaps.geolocation;
-	document.getElementById("country").innerHTML = o.country;
-    if (o.country == "Россия" || o.country == "Беларусь"){
-        window.location.href='ru.html';
-    } else {
-        window.location.href='en.html';
-    }
-}

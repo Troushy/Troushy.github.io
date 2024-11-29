75a1c994c74320681dc84f59af5d332964f6deb3
UralStuff <dejdevatarovzena@gmail.com>
2024-11-29 22:10:52 +0500

    v5.12.3.2-01

--- langswitch.js
+++
@@ -1,5 +0,0 @@
-var select = document.getElementById("language");
-select.addEventListener('change', function (e) {
-    // alert(e.target.value + '.html');
-    window.location.href = e.target.value + '.html';
-})
